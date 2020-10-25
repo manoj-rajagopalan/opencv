7f0c26575258fd153d5447c8a1b3b76cf4a71d0d
Manoj Rajagopalan <rajagopalan.manoj@gmail.com>
2020-10-25 05:02:22 +0000

    img_dims: write out elemSize1 and channels

--- img_dims.cpp
+++ img_dims.cpp
@@ -9,6 +9,8 @@ int main(int argc, char *argv[])
 	cv::Mat img = cv::imread(argv[1]);
 	std::cout << "img.shape = " << img.cols << 'x' << img.rows << std::endl;
 	std::cout << "img.dims = " << img.dims << std::endl;
+	std::cout << "img.elemSize1 = " << img.elemSize1() << std::endl;
+	std::cout << "img.channels = " << img.channels() << std::endl;
 	std::cout << "img.elemSize = " << img.elemSize() << std::endl;
 	std::cout << "img.depth (code) = " << img.depth() << std::endl;
 	return 0;

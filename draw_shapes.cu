8e955ce76fcb7f04ca3950340b491af6f5c90433
Manoj Rajagopalan <rajagopalan.manoj@gmail.com>
2021-04-05 23:07:48 -0700

    Initial cut

---
+++ draw_shapes.cu
@@ -0,0 +1,45 @@
+#include "cuda.h"
+
+__device__ __forceinline__ int sqr(int const x) { return (x * x); }
+
+// Call with 2D thread-organization. Could be called with 1D arrangement but this is for exercise.
+__global__ void clearFrame(uchar3 *const frame, int const frame_width, int const frame_height)
+{
+    int const x = blockIdx.x * blockDim.x + threadIdx.x;
+    int const y = blockIdx.y * blockDim.y + threadIdx.y;
+    if(x < frame_width && y < frame_height) {
+        int const row_size = gridDim.x * blockDim.x;
+        int const offset = y * row_size + x;
+        frame[offset][0] = 0u;
+        frame[offset][1] = 0u;
+        frame[offset][2] = 0u;
+    }
+}
+
+__global__ void drawCircle(int const x0, int const y0, int const radius, uchar3 const color,
+                           uchar3 *const frame, int const frame_width, int const frame_height)
+{
+    int const x = x0 - (threadIdx.x - blockDim.x / 2);
+    int const y = y0 - (threadIdx.y - blockDim.y / 2);
+    if(x >= x0 && x < frame_width && y >= 0 && y < frame_height) {
+        int const hypot_sqr = sqr(x - x0) + sqr(y - y0);
+        if(hypot_sqr <= sqr(radius)) {
+            uchar3 *const pixel = frame + y * frame_width + x;
+            *pixel = color;
+        }
+    }
+}
+
+__global__ void drawRect(int const x0, int const y0,
+                         int const x1, int const y1,
+                         uchar3 const color,
+                         uchar3 *const frame, int const frame_width, int const frame_height)
+ {
+     int const x = x0 + threadIdx.x;
+     int const y = y0 + threadIdx.y;
+     
+     if(x >= 0 && x < frame_width && y >= 0 && y < frame_height) {
+        uchar3 *const pixel = frame + y * frame_width + x;
+        *pixel = color;
+     }
+ }
---
+++ video_frame_generator_cuda.cu
@@ -0,0 +1,139 @@
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <sstream>
+
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/videoio.hpp"
+
+#include "shapes.hpp"
+#include "moving_object.hpp"
+
+#include "draw_shapes.cu"
+
+int constexpr FRAME_WIDTH = 960;
+int constexpr FRAME_HEIGHT = 600;
+
+inline int sqr(int const x) { return x * x; }
+
+std::vector<MovingObject> generateCircles(int num)
+{
+    std::vector<MovingObject> objects;
+    std::mt19937_64 rand_engine;
+    std::uniform_real_distribution<float> rand_gen;
+    for(int n = 0; n < num; ++n) {
+        std::array<uchar,3> rand_color;
+        rand_color[0] = uchar(255 * rand_gen(rand_engine));
+        rand_color[1] = uchar(255 * rand_gen(rand_engine));
+        rand_color[2] = uchar(255 * rand_gen(rand_engine));
+
+        Path rand_path;
+        rand_path.origin[0] = FRAME_WIDTH / 2 + int((FRAME_WIDTH * (rand_gen(rand_engine) - 0.5)));
+        rand_path.origin[1] = FRAME_HEIGHT / 2 + int((FRAME_HEIGHT * (rand_gen(rand_engine) - 0.5)));
+        rand_path.step[0] = int(4 * (rand_gen(rand_engine) - 0.5));
+        rand_path.step[1] = int(4 * (rand_gen(rand_engine) - 0.5));
+
+        int const radius = int(75 * rand_gen(rand_engine));
+        objects.emplace_back(MovingObject::MakeCircle(radius, rand_color, rand_path));
+    }
+    return objects;
+}
+
+std::vector<MovingObject> generateRects(int num)
+{
+    std::vector<MovingObject> objects;
+    std::mt19937_64 rand_engine;
+    std::uniform_real_distribution<float> rand_gen;
+    for(int n = 0; n < num; ++n) {
+        std::array<uchar,3> rand_color;
+        rand_color[0] = uchar(255 * rand_gen(rand_engine));
+        rand_color[1] = uchar(255 * rand_gen(rand_engine));
+        rand_color[2] = uchar(255 * rand_gen(rand_engine));
+
+        Path rand_path;
+        rand_path.origin[0] = FRAME_WIDTH / 2 + int((FRAME_WIDTH * (rand_gen(rand_engine) - 0.5)));
+        rand_path.origin[1] = FRAME_HEIGHT / 2 + int((FRAME_HEIGHT * (rand_gen(rand_engine) - 0.5)));
+        rand_path.step[0] = int(4 * (rand_gen(rand_engine) - 0.5));
+        rand_path.step[1] = int(4 * (rand_gen(rand_engine) - 0.5));
+
+        int const width = int(80 * rand_gen(rand_engine));
+        int const height = int(80 * rand_gen(rand_engine));
+        objects.emplace_back(MovingObject::MakeRect(width, height, rand_color, rand_path));
+    }
+    return objects;
+}
+
+void cudaCheckSuccess(cudaError_t const cuda_status, std::string const& message)
+{
+    if(cudaSuccess != cuda_status) {
+        std::cout << "CUDA ERROR " << cuda_status << ": " << message << std::endl;
+        std::cout << "- " << cudaGetErrorName(cuda_status) << ": " << cudaGetErrorString(cuda_status) << std::endl;
+    }
+}
+
+void generateFrames(std::vector<MovingObject> const& circles,
+                    std::string const& prefix)
+{
+    cv::Mat frame(FRAME_HEIGHT, FRAME_WIDTH, CV_8UC3);
+    int const frame_num_bytes = FRAME_HEIGHT * FRAME_WIDTH * 3;
+
+    cudaError_t cuda_status = cudaSuccess;
+
+    // CUDA stream
+    cudaStream_t cuda_stream;
+    cuda_status = cudaStreamCreate(&cuda_stream);
+    cudaCheckSuccess(cuda_status, "Error creating CUDA stream");
+
+    // Allocate frame on device
+    uchar3 *dev_frame = nullptr;
+    cuda_status = cudaMalloc((void**) &dev_frame, frame_num_bytes);
+    cudaCheckSuccess(cuda_status, "Error allocating frame in device");
+
+    for(int i_frame = 0; i_frame < 500; ++i_frame) {
+        std::cout << "Drawing frame " << i_frame << '\n';
+
+        // Clear frame
+        dim2 clear_block(16,16);
+        dim2 clear_grid((FRAME_WIDTH+15)/16, (FRAME_HEIGHT+15)/16);
+        clearFrame<<<clear_grid, clear_block, 0, cuda_stream>>>(dev_frame, FRAME_WIDTH, FRAME_HEIGHT);
+        cuda_status = cudaGetLastError();
+        cudaCheckSuccess(cuda_status, "Error launching 'clear' kernel in frame " + std::to_string(i_frame));
+
+        // Draw circles
+        int circle_counter = 0;
+        for(auto &obj : circles) {
+            Circle const *const c = dynamic_cast<Circle const*>(obj.shape.get());
+            assert(c);
+            dim2 const circle_block(2 * c->radius + 10, 2 * c->radius + 10);
+            uchar3 const color{c->color[0], c->color[1], c->color[2]};
+            drawCircle<<<1, circle_block, 0, cuda_stream>>>(obj.x, obj.y, c->radius, color, dev_frame, FRAME_WIDTH, FRAME_HEIGHT);
+            cuda_status = cudaGetLastError();
+            cudaCheckSuccess(cuda_status, "Error launching kernel for circle " + std::to_string(circle_counter) + " in frame " + std::to_string(i_frame));
+            obj.update();
+            ++circle_counter;
+        }
+
+        // Fetch frame from GPU and write to img file
+        cuda_status = cudaMemcpy((void*) frame.data, (void*) dev_frame, frame_num_bytes, cudaMemcpyDeviceToHost);
+        cudaCheckSuccess(cuda_status, "Error copying data out of GPU in frame " + std::to_string(i_frame));
+        std::ostringstream s;
+        s << prefix << '-' << std::setw(3) << std::setfill('0') << i_frame << ".png";
+        cv::imwrite(s.str(), frame);
+    }
+
+    cudaFree((void*) dev_frame);
+}
+
+int main(int const argc, char const *argv[])
+{
+    assert(argc == 2 && "Usage: video_frame_generator <prefix>-nnn.png");
+    std::vector<MovingObject> circles = generateCircles(50);
+    generateFrames(circles, argv[1]);
+    return 0;
+}
